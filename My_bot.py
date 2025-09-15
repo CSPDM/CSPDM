fcb25b996f9956ebc5d2231fb0cfe53118a6ad18
CSPDM <cspdm.top@gmail.com>
2025-09-15 05:29:29 +0300

    Update My_bot.py

--- My_bot.py
+++ My_bot.py
@@ -10,7 +10,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
     level=logging.INFO
 )
-logger = logging.getLogger(_name_)
+logger = logging.getLogger(__name__)
 
 BOT_TOKEN = "7674783654:AAEsfosyZs40Aklk8hzB5L6fWMuiNQXa73o"
 
